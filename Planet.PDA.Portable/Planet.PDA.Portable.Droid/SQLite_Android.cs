e91a538394393065b89ca90311779453edb9964f
tachikawa <tachikawa@cybergrow.co.jp>
2016-10-07 17:50:08 +0900

    SqlitePCL追加

---
+++ Planet.PDA.Portable/Planet.PDA.Portable.Droid/SQLite_Android.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using SQLite.Net;
+using SQLite.Net.Platform.XamarinAndroid;
+using XamarinApp.Droid;
+using Planet.PDA.Portable;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLite_Android))]
+namespace XamarinApp.Droid
+{
+    public class SQLite_Android : ISQLite
+    {
+        public SQLiteConnection GetConnection()
+        {
+            return new SQLiteConnection(new SQLitePlatformAndroid(), FilePath);
+        }
+
+        public string FilePath
+        {
+            get
+            {
+                return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "SQLite.db3");
+            }
+        }
+    }
+}
